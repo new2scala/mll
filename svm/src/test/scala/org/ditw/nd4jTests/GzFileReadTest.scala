887074e6a6e6e3ad3b31bac3ab324c14fa879faa
new2scala <jiajiwu.icare@gmail.com>
2017-05-24 03:16:08 +0200

    t

--- svm/src/test/scala/org/ditw/nd4jTests/GzFileReadTest.scala
+++ svm/src/test/scala/org/ditw/nd4jTests/GzFileReadTest.scala
@@ -1,6 +1,6 @@
 package org.ditw.nd4jTests
 
-import java.io.{InputStream, InputStreamReader, StringReader}
+import java.io.{FileOutputStream, InputStream, InputStreamReader, StringReader}
 import java.nio.charset.StandardCharsets
 
 import org.apache.commons.io.IOUtils
@@ -12,9 +12,7 @@ import scala.collection.mutable.ListBuffer
   * Created by dev on 2017-05-23.
   */
 object GzFileReadTest extends App {
-  val path = "/media/sf_work/tmp/latest-all.json.bz2"
-
-  def reader(fn:String, is:InputStream):String = {
+  def reader(fn:String, is:InputStream, lineCount:Int):String = {
     //var start = 0
     val bufSize = 1024
 
@@ -22,18 +20,33 @@ object GzFileReadTest extends App {
 
       val buf = new Array[Char](bufSize)
       val reader = new InputStreamReader(is, StandardCharsets.UTF_8)
+    var currLine = ListBuffer[String]()
       var l = reader.read(buf)
-      while (l > 0 && lines.size < 100) {
-        lines += buf.slice(0, l).mkString
+      while (l > 0 && lines.size < lineCount) {
+        val r = buf.slice(0, l).mkString
+        val nl = r.indexOf('\n')
+        if (nl >= 0) {
+          currLine += r.substring(0, nl+1)
+          lines += currLine.mkString
+          currLine = ListBuffer[String]()
+          currLine += r.substring(nl+1)
+        }
+        else {
+          currLine += r
+        }
         l = reader.read(buf)
       }
-    lines.mkString
+
       //start = start + l
 
-
+    lines.mkString
   }
 
+  def reader20(fn:String, is:InputStream) = reader(fn, is, 20)
+
+  //val path = "/media/sf_work/tmp/latest-all.json.bz2"
+  val path = "/media/sf_work/tmp/enwiki-latest-pages-articles1.xml-p10p30302.bz2"
   //val s = TgzUtils.processGzFile(path, reader)
-  val s = TgzUtils.processBzip2File(path, reader)
-  println(s)
+  val s = TgzUtils.processBzip2File(path, reader20)
+  IOUtils.write(s, new FileOutputStream("/media/sf_work/tmp/articles1.xml.part"), StandardCharsets.UTF_8)
 }
